3723d72035138b1604220754eb00489835217b6c
Richard Cox <cox.richard200@gmail.com>
2025-09-22 12:55:29 -0500

    migrating to oclif

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,36 +1,9 @@
-// @ts-check
-import eslint from '@eslint/js'
-import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended'
-import globals from 'globals'
-import { defineConfig } from 'eslint/config'
-import tseslint from 'typescript-eslint'
+import {includeIgnoreFile} from '@eslint/compat'
+import oclif from 'eslint-config-oclif'
+import prettier from 'eslint-config-prettier'
+import path from 'node:path'
+import {fileURLToPath} from 'node:url'
 
-export default defineConfig(
-  {
-    ignores: ['eslint.config.mjs'],
-  },
-  eslint.configs.recommended,
-  ...tseslint.configs.recommendedTypeChecked,
-  eslintPluginPrettierRecommended,
-  {
-    languageOptions: {
-      globals: {
-        ...globals.node,
-        ...globals.jest,
-      },
-      ecmaVersion: 5,
-      sourceType: 'module',
-      parserOptions: {
-        projectService: true,
-        tsconfigRootDir: process.cwd(),
-      },
-    },
-  },
-  {
-    rules: {
-      '@typescript-eslint/no-explicit-any': 'off',
-      '@typescript-eslint/no-floating-promises': 'warn',
-      '@typescript-eslint/no-unsafe-argument': 'warn'
-    },
-  },
-)
+const gitignorePath = path.resolve(path.dirname(fileURLToPath(import.meta.url)), '.gitignore')
+
+export default [includeIgnoreFile(gitignorePath), ...oclif, prettier]
