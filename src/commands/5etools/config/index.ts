d1fc7cf5646aec7487d74bded3a3760090acf0a8
Richard Cox <rick.cox@ssmhealth.com>
2025-09-24 08:05:40 -0500

    beginning work on 5etools command

---
+++ src/commands/5etools/config/index.ts
@@ -0,0 +1,24 @@
+import { Command } from "@oclif/core"
+import { Octokit } from "octokit";
+
+const octokit = new Octokit()
+
+export async function fetch5eToolsData(): Promise<any> {
+  const response = await octokit.request('GET /repos/5etools-mirror-3/5etools-src/contents/data', {
+    headers: {
+      'Accept': 'application/vnd.github.v3.object+json',
+      'X-GitHub-Api-Version': '2022-11-28'
+    }
+  });
+  return response.data;
+}
+
+export class FiveEToolsConfig extends Command {
+  static description = "Configure 5etools settings"
+
+  async run(): Promise<void> {
+    const data = await fetch5eToolsData()
+    console.log(data)
+    this.log('5etools config command executed')
+  }
+}
---
+++ src/commands/5etools/index.ts
@@ -0,0 +1,9 @@
+import { Command } from "@oclif/core"
+
+export class FiveETools extends Command {
+  static description = "Command to process 5etools data"
+
+  async run(): Promise<void> {
+    this.log('5etools command executed')
+  }
+}
