52f8411360a74453bdfe2f5c6f3b66b33e843fac
Richard Cox <cox.richard200@gmail.com>
2025-09-24 12:07:56 -0500

    updating eslint and prettier config

--- src/commands/5etools/config/index.ts
+++ src/commands/5etools/config/index.ts
@@ -1,20 +1,20 @@
-import { Command } from "@oclif/core"
-import { Octokit } from "octokit";
+import { Command } from '@oclif/core'
+import { Octokit } from 'octokit'
 
 const octokit = new Octokit()
 
-export async function fetch5eToolsData(): Promise<any> {
+export async function fetch5eToolsData(): Promise<unknown> {
   const response = await octokit.request('GET /repos/5etools-mirror-3/5etools-src/contents/data', {
     headers: {
-      'Accept': 'application/vnd.github.v3.object+json',
-      'X-GitHub-Api-Version': '2022-11-28'
-    }
-  });
-  return response.data;
+      Accept: 'application/vnd.github.v3.object+json',
+      'X-GitHub-Api-Version': '2022-11-28',
+    },
+  })
+  return response.data
 }
 
 export class FiveEToolsConfig extends Command {
-  static description = "Configure 5etools settings"
+  static description = 'Configure 5etools settings'
 
   async run(): Promise<void> {
     const data = await fetch5eToolsData()
--- src/commands/5etools/index.ts
+++ src/commands/5etools/index.ts
@@ -1,7 +1,7 @@
-import { Command } from "@oclif/core"
+import { Command } from '@oclif/core'
 
 export class FiveETools extends Command {
-  static description = "Command to process 5etools data"
+  static description = 'Command to process 5etools data'
 
   async run(): Promise<void> {
     this.log('5etools command executed')
